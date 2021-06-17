f8f0c9904a41e028aeb4253c661c8e65c9d5d554
sieri <sieri@windowslive.com>
2021-06-17 15:54:16 +0200

    Base of display stucture

--- Calibration.py
+++ Calibration.py
@@ -1,23 +1,13 @@
 from Capture.CameraCalibration import CamCalib
 import tkinter as tk
 
-
-
-
+calib = CamCalib()
 
 def textBased():
     print("=====Camera Calibration script====")
 
     string = str(input("enter camera address:"))
 
-    print("attempting to open camera")
-    calib.set_access(string)
-
-    if not calib.activate_camera():
-        print("Couldn't open camera")
-        exit(-1)
-
-    calib.show_camera()
 
     answer = str(input("Confirm correct camera [Y/N]:", ))
 
@@ -30,22 +20,32 @@ def textBased():
 
 
 def event_btn_confirm(event):
-    print(ent_camera_ip.get())
+
+    print("attempting to open camera")
+    calib.set_access(ent_camera_ip.get())
+
+    if not calib.activate_camera():
+        print("Couldn't open camera")
+        exit(-1)
+
+    calib.show_camera()
 
 
 if __name__ == '__main__':
     window = tk.Tk(screenName="Calibration Window")
 
+    # create widgets
     lbl_camera_IP = tk.Label(text="Enter camera address:")
     ent_camera_ip = tk.Entry()
     btn_camera_ip = tk.Button(text="Confirm")
 
+    # place on the window
     lbl_camera_IP.pack()
     ent_camera_ip.pack()
     btn_camera_ip.pack()
 
+    # bind functionalities
     btn_camera_ip.bind("<Button-1>", event_btn_confirm)
-
 
     window.mainloop()
 
--- Capture/CameraDisplay.py
+++ Capture/CameraDisplay.py
@@ -2,13 +2,12 @@
 
 from threading import Thread
 import cv2 as cv
+from PIL import Image, ImageTk
 
+class DisplayBase:
 
-class Display:
-
-    def __init__(self, win_name : str, capure):
-        self.win_name = win_name
-        self.capture = capure
+    def __init__(self, capture):
+        self.capture = capture
         self.stopped = False
 
     def start(self):
@@ -21,10 +20,33 @@ class Display:
         while not self.stopped:
             grabbed, image = self.capture.read()
             if grabbed:
-                cv.imshow(self.win_name, image)
+                self.show(image)
                 cv.waitKey(1)
             else:
                 self.stop()
 
+    def show(self, image):
+        pass
+
     def stop(self):
         self.stopped = True
+
+class Display(DisplayBase):
+
+    def __init__(self, win_name : str, capture):
+        super().__init__(capture)
+        self.win_name = win_name
+
+    def show(self, image):
+        cv.imshow(self.win_name, image)
+
+
+class TkDisplay(DisplayBase):
+    def __init__(self, widget, capture):
+        super().__init__(capture)
+        self.widget = widget
+
+    def show(self, image):
+        rgb = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+        img = Image.fromarray(rgb)
+        self.widget = ImageTk.PhotoImage(img)
