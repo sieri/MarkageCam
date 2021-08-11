d714b7d26f0f3906908b4cc4d66372f15933348b
sieri <sieri@windowslive.com>
2021-08-11 12:53:34 +0200

    Prepare for deployement

--- environement.py
+++ environement.py
@@ -3,14 +3,13 @@ File setting up global variables for the system, describe the running condition
 """
 
 debug = True  # if true more printouts in the code
-test_setup = True  # if true indicates we are in a test setup and call the simulators for external hardware or software
+test_setup = False  # if true indicates we are in a test setup and call the simulators for external hardware or software
 save_to_db = True  # if true the images are saved to the database and local file system, if false they aren't
 image_path = "./img/"  # path from current folder where images are stored
 
 
 pulling_time_syncro = 0.100  # time between read of the synchronisation value
-display_fps = 24 # number of framed pulled per second
-
+display_fps = 24  # number of framed pulled per second
 
 
 # set the correct opc server in test
