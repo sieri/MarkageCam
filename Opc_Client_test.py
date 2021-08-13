b5bd63bb09547f3088b1a1b1e9b3755ed717bec0
sieri <sieri@windowslive.com>
2021-08-13 12:16:23 +0200

    Update Opc_Client_test.py

--- Opc_Client_test.py
+++ Opc_Client_test.py
@@ -1,7 +1,8 @@
 from Capture import Opc_Client
-from Capture.Opc_Client import OpcClient, address_text,address_trigger
+from Capture.Opc_Client import OpcClient, address_text,address_trigger, set_synchro, get_text, kill_synchro, get_repetions
 from environement import default_opc_server, test_setup
 import OpenOPC
+import time 
 
 def callback():
     print("Taking picture now")
@@ -15,15 +16,17 @@ if __name__ == '__main__':
         if test_setup:
             address_text = "." + address_text
             address_trigger = "." + address_trigger
-            with OpcClient('Matrikon.OPC.Simulation') as opc:
-                opc.opc[address_text] = 'test'
+
+        with OpcClient(default_opc_server) as opc:
+            opc.opc[address_text] = 'test42'
 
         set_synchro(callback)
 
-        for i in range(60):
+        for i in range(20):
             time.sleep(1)
             string = get_text()
             print('Reading the text', string)
+            print('repeats:', get_repetions())
 
         kill_synchro()
         print("end test")
