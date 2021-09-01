1948dc84bf8db28275791bfce24c9d7e8dcaef06
sieri <sieri@windowslive.com>
2021-09-01 15:44:10 +0200

    order of the environement variable changed

--- environement.py
+++ environement.py
@@ -7,6 +7,7 @@ test_setup = True  # if true indicates we are in a test setup and call the simul
 save_to_db = True  # if true the images are saved to the database and local file system, if false they aren't
 image_path = "./img/"  # path from current folder where images are stored
 use_simulator = True  # if true will use the simulator for the capture synchronisation
+max_img_bytes = 52428800 # maximum size a distorted image can have in byte
 
 pulling_time_syncro = 0.100  # time between read of the synchronisation value
 display_fps = 5  # number of framed pulled per second
@@ -14,7 +15,6 @@ display_fps = 5  # number of framed pulled per second
 fish_eye_correction = False # if true active the fisheye correction
 fish_eye_parametter = 'cameraMatrice.json'
 
-max_img_bytes = 52428800
 
 # set the correct opc server in test
 if not test_setup:
